a396ea253fb0792a1782866a152da7964b1c03d3
Aditya Verma <aaadarths2357@gmail.com>
2020-10-05 09:47:49 +0530

    Selected Inputs

--- src/main/java/com/bridgelabz/workshop/tictactoeprogram/TicTacToeGame.java
+++ src/main/java/com/bridgelabz/workshop/tictactoeprogram/TicTacToeGame.java
@@ -1,16 +1,44 @@
 package com.bridgelabz.workshop.tictactoeprogram;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TicTacToeGame {
-	
-	public static void addBoard() {
-		char[] board=new char[10];
-		for(int i=0;i<10;i++)
-			board[i]=' ';			
+
+//UC1 :	ADD BOARD
+	private static char[] addBoard() {
+		char[] board = new char[10];
+		Arrays.fill(board, ' ');
+		return board;
 	}
-	
+//UC2 : SELECT INPUT
+	private static char[] selectInput() {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Choose X or O :");
+		char choice=sc.next().charAt(0);
+		char userInput=' ';
+		char computerInput=' ';
+		switch(choice) {
+			case 'X':
+				userInput=choice;
+				computerInput='O';
+				break;
+			case 'O':
+				userInput=choice;
+				computerInput='X';
+				break;
+			default:
+				System.out.println("Invalid Input chosen");
+		}
+		char[] inputs= {userInput,computerInput};
+		return inputs;
+		
+	}
+
 	public static void main(String[] args) {
-		TicTacToeGame gameObj=new TicTacToeGame();
-		gameObj.addBoard();
+		TicTacToeGame gameObj = new TicTacToeGame();
+		char[] newBoard = gameObj.addBoard();
+		char[] inputs=gameObj.selectInput();			
 	}
 
 }
